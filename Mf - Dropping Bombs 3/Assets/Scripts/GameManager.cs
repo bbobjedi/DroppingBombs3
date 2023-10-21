a8a95928096f644d93266e666982bb68441691ab
MasterHacker10 <noahcho78@gmail.com>
2023-10-21 12:24:30 -0400

    code
    code

--- Mf - Dropping Bombs 3/Assets/Scripts/GameManager.cs
+++ Mf - Dropping Bombs 3/Assets/Scripts/GameManager.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+    private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+
+    void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawner.active = false;
+        title.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!gameStarted)
+        {
+            if(Input.anyKeyDown)
+
+            {
+                ResetGame();
+            }
+        } else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
+        if (Input.anyKeyDown)
+        {
+            spawner.active = true;
+            title.SetActive(false);
+        }
+        var nextBomb = GameObject.FindGameObjectWithTag("Bomb");
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted)
+            {
+                Destroy(bombObject);
+            }
+        }
     }
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+    }
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+        splash.SetCtive(true);
+    }
+
 }
